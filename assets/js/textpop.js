3b762a817bcc103427d4f42ff77c8f1f396efbcd
Griea <grieantaylor@gmail.com>
2019-05-31 12:02:11 +1000

    trial

--- assets/js/textpop.js
+++ assets/js/textpop.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
 	// don't delete me
     
- $('.2002girl').mouseenter(function() {
-    $('.2002').addClass('textshow');
+ $('.girl2002').mouseenter(function() {
+    $('.text2002').addClass('textshow');
+  });
+
+  $('.girl2002').mouseleave(function() {
+    $('.text2002').removeClass('textshow');
   });
 
 
