2544124a8172a5081d97414ef2521ee8e56ceb40
Griea <grieantaylor@gmail.com>
2019-05-31 10:42:16 +1000

    Changes

---
+++ assets/js/video.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+	// don't delete me
+    
+ $('.eat').mouseenter(function() {
+    $('.videat').addClass('vidshow');
+  });
+
+$('.eat').mouseleave(function() {
+    $('.videat').removeClass('vidshow');
+  });
+
+
+
+ $('.slip').mouseenter(function() {
+    $('.vidslip').addClass('vidshow');
+  });
+
+$('.slip').mouseleave(function() {
+    $('.vidslip').removeClass('vidshow');
+  });
+
+
+ $('.calls').mouseenter(function() {
+    $('.vidcall').addClass('vidshow');
+  });
+
+
+ $('.juicevid').mouseleave(function() {
+    $('.vidjuice').removeClass('vidshow');
+  });
+
+
+
+
+// don't delete me
+});
