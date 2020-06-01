67e9a2d67c6e5b05419635a26b0766be407daeaa
Griea <grieantaylor@gmail.com>
2020-06-01 20:24:39 +1000

    p5 add

---
+++ assets/js/sketch.js
@@ -0,0 +1,21 @@
+var canvas;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position (0, 0);
+  canvas.style('z-index', '1')
+  noFill()
+  ;
+}
+
+function mousePressed() {
+	clear();
+}
+
+function draw() {
+  fill (mouseX, mouseY, 240);
+  noStroke (
+
+);
+ ellipse(pmouseX, pmouseY, 20, 20 );
+}
